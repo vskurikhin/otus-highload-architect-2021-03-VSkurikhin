49e0b4f6bd156c745babea21f7a1e3a916e9f258
Victor N. Skurikhin <VNSkurikhin@sberbank.ru>
2021-04-05 16:09:14 +0300

    Добавлен файл inject_server.go. Обновленны go.mod и go.sum.

---
+++ cmd/inject_server.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/fate-lovely/phi"
+	"github.com/google/wire"
+	"hl.svn.su/highload-architect/app/config"
+	"hl.svn.su/highload-architect/app/server"
+)
+
+// набор wire проводов для загрузки сервера.
+var serverSet = wire.NewSet(
+	provideRouter,
+	provideServer,
+)
+
+// provideServer это функция провайдера Wire, которая возвращает
+// http сервер, настроенный из среды окружения (environment).
+func provideServer(handler phi.Handler, config config.Config) *server.Server {
+	return &server.Server{
+		Host:    config.Server.Host,
+		Handler: handler,
+	}
+}
+
+// provideRouter это функция провайдера Wire, которая возвращает маршрутизатор,
+// обслуживающий предоставленные обработчики.
+func provideRouter() phi.Handler {
+	return nil
+}
