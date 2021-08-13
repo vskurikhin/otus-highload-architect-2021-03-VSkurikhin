87ff0a92ab97256ace118cc8b1ec870a35ba333e
Victor N. Skurikhin <vskurikhin@gmail.com>
2021-08-13 11:17:33 +0300

    Выделение общей библиотеки.

---
+++ app-lib/domain/dialog_message.go
@@ -0,0 +1,59 @@
+package domain
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/savsgio/go-logger/v2"
+)
+
+type DialogMessage struct {
+	Id       uint64
+	ShardId  uint8
+	FromUser uint64
+	ToUser   uint64
+	Message  string
+	ParentId *uint64
+	HashId   uint8
+}
+
+func (d *DialogMessage) String() string {
+	return string(d.Marshal())
+}
+
+func (d *DialogMessage) Marshal() []byte {
+
+	session, err := json.Marshal(*d)
+	if err != nil {
+		logger.Error(err)
+		return nil
+	}
+	return session
+}
+
+func (d *dialogMessage) Create(shardId uint8, dm *DialogMessage) error {
+	return d.create(dm, shardId)
+}
+
+const INSERT_INTO_DIALOG_MESSAGE = "INSERT INTO dialog_message " +
+	" (shard_id, id, from_user, to_user, message, parent_id, hash_id) " +
+	"  VALUES " +
+	" (%d, ?, ?, ?, ?, ?, ?)"
+
+func (d *dialogMessage) create(dm *DialogMessage, shardId uint8) error {
+	// Подготовить оператор для вставки данных
+	sql := fmt.Sprintf(INSERT_INTO_DIALOG_MESSAGE, shardId)
+	logger.Debugf("create sql: %s", sql)
+	stmtIns, err := d.dbRw.Prepare(sql) // ? = заполнитель
+
+	if err != nil {
+		return err // правильная обработка ошибок вместо паники
+	}
+	defer func() { _ = stmtIns.Close() }() // Закрывается оператор, когда выйдете из функции
+	_, err = stmtIns.Exec(dm.Id, dm.FromUser, dm.ToUser, dm.Message, dm.ParentId, dm.HashId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
